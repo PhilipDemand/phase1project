cc7ff61a8f41804b5be7c63654cd2b73893dbb3e
Philip Demand <philipdemand@yahoo.com>
2023-02-06 23:07:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+This web app is a community tool-share.  Tools are expensive, and sometimes a single use does not justify a purchase. 
+
+"Somebody nearby must have this specialty tool.  If only there was a way to borrow one!"
+"I've got this really useful tool that just sits dormant.  And I bet somebody nearby could get some use out of it."
+
+Instead of a tool library that would require a physical space to store available tools, this app would show tools that people own that they have made available to the community to borrow.  This app lets people submit tools that they want to allow people to borrow, and lets people reserve one that is available.
+
+Functionality - 
+
+Upon visiting:
+    The app fetches all the tools from db.json and renders them into individual "cards."
+    Each card shows the tool's name, an image of the tool, and it's availability status; either "Available" or "Reserved."
+    A dropdown menu is populated with the names of only the tools with the status "Available."
+    Hovering over a tool's card with make a description appear.
+
+Reserving a tool:
+    To reserve a tool, select the desired tool from the dropdown menu, and click the "Reserve Tool" button.
+    An alert will pop up with a confirmation of the reservation, and a phone number to call to arrange the pickup and dropoff.
+    The status of that tool in its card will change to "Reserved."
+    The name of the reserved tool wil disappear from the dropdown menu.
+
+Submitting a tool:
+    Type the name of your tool in the first field of the form, an image URL in the next, and your phone number in the last.
+    Upon clicking the "Submit Tool" button, your tool will now appear in its own rendered card with the status "Available."
+    The name of the submitted tool will be added to the dropdown menu.
+    No new card will be rendered if the submit button is clicked while the form fields are empty.
---
+++ index.js
@@ -0,0 +1,111 @@
+const toolCollectionDiv = document.querySelector("#tool-collection")
+const selectDropDown = document.getElementById("selectToolMenu");
+const nameNumberArray = []
+const collectImages = document.getElementsByClassName("tool-avatar");
+
+function fetchAndRenderTools () {
+fetch("http://localhost:3000/tools")
+.then(response => response.json())
+.then( (someData) => {
+  const arrayOfToolObjects = someData
+  toolCollectionDiv.innerHTML = ""
+  
+  const justAvailableNames = arrayOfToolObjects.filter(status => status.availability === "Available");
+  const namesArray = justAvailableNames.map(each => each.name);
+  
+  selectDropDown.innerHTML = "";
+  for (var j = 0; j < namesArray.length; j++) {
+    let option = document.createElement("option");
+    option.text = namesArray[j];
+    option.value = namesArray[j];
+    selectDropDown.add(option);
+  }
+
+  for (k = 0; k < arrayOfToolObjects.length; k++) {
+    nameNumberArray.push(`${arrayOfToolObjects[k].name} : ${arrayOfToolObjects[k].id} : ${arrayOfToolObjects[k].phonenumber}`)
+  }
+
+  arrayOfToolObjects.map(  
+    
+    (eachToolObject)=>{
+      const divForToolCard = document.createElement("div")
+        divForToolCard.className = "card"
+      const h2ForToolCard = document.createElement("h2")
+        h2ForToolCard.textContent = eachToolObject.name
+      const imageForToolCard = document.createElement("img")
+        imageForToolCard.src = eachToolObject.image
+        imageForToolCard.className = "tool-avatar"
+       const pTagForToolCard = document.createElement("p")
+         pTagForToolCard.textContent = `${eachToolObject.availability}`
+         const pTagForDescription = document.createElement("p")
+         pTagForDescription.textContent = eachToolObject.useDescription
+         pTagForDescription.style.display = "none"
+      divForToolCard.addEventListener("mouseover", function() {
+       pTagForDescription.style.display = "block"
+      })
+      divForToolCard.addEventListener("mouseleave", function() {
+        pTagForDescription.style.display = "none"
+      })
+      divForToolCard.append(h2ForToolCard , imageForToolCard , pTagForToolCard, pTagForDescription)
+
+      toolCollectionDiv.append(divForToolCard)
+  })})}
+  
+fetchAndRenderTools()
+
+const selectButton = document.getElementById("reserveToolButton");
+  selectButton.addEventListener("click", function() {
+    const item = selectDropDown.value
+    const filteredArray = nameNumberArray.filter(string => string.includes(item));
+    let last12 = filteredArray[0].substr(filteredArray[0].length - 12);
+    alert(`You have successfully reserved a ${selectDropDown.value}\nPlease call ${last12} to arrange a pickup and dropoff`);
+    
+    let filteredId = filteredArray[0].charAt(filteredArray[0].length - 16);
+    
+    fetch(`http://localhost:3000/tools/${filteredId}`, {
+    method: 'PATCH',
+    headers: {
+    'Content-Type': 'application/json'
+  },
+    body: JSON.stringify({
+      availability: 'Reserved'
+  })
+})
+  .then(response => response.json())
+  .catch(error => console.error(error));
+   fetchAndRenderTools()
+  }
+  );
+
+  const submitForm = document.getElementById("submitToolForm");
+
+  submitForm.addEventListener("submit", function(e) {
+    
+    const formElements = e.target.elements;
+
+    for (let i = 0; i < formElements.length; i++) {
+      if (!formElements[i].value) {
+        e.preventDefault();
+        return;
+      }
+    }
+   
+    const submittedObject = {
+      name:e.target[0].value, 
+      image:e.target[1].value,
+      availability:"Available",
+      phonenumber:e.target[2].value,
+      useDescription:e.target[3].value
+    }
+
+    fetch("http://localhost:3000/tools", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json", 
+        Accept: "application/json"
+      },
+      body: JSON.stringify(submittedObject)
+    })
+    .then(response => response.json())
+    .then( () => fetchAndRenderTools())
+  })
