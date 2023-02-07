de956bbbe8a3d4b097a3869de1c361da7b46aab5
Philip Demand <philipdemand@yahoo.com>
2023-02-07 12:02:50 -0500

    clean index.js and comments

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const toolCollectionDiv = document.querySelector("#tool-collection")
 const selectDropDown = document.getElementById("selectToolMenu");
 const nameNumberArray = []
-const collectImages = document.getElementsByClassName("tool-avatar");
 
+// fetch tools from db.json and render each one into it's own card
 function fetchAndRenderTools () {
 fetch("http://localhost:3000/tools")
 .then(response => response.json())
 .then( (someData) => {
+  
+  //make the json data usable in this funciton
   const arrayOfToolObjects = someData
   toolCollectionDiv.innerHTML = ""
   
+  //creates an array of the tools that have the status of "Available"
   const justAvailableNames = arrayOfToolObjects.filter(status => status.availability === "Available");
+  //takes the complete tool from justAvailableNames and makes it into an array of just the names of the tools
   const namesArray = justAvailableNames.map(each => each.name);
   
+ //populate the dropdown menu with the names of the available tools
   selectDropDown.innerHTML = "";
   for (var j = 0; j < namesArray.length; j++) {
     let option = document.createElement("option");
@@ -21,10 +26,12 @@ fetch("http://localhost:3000/tools")
     selectDropDown.add(option);
   }
 
+  //makes the name, id number, and phone number of each tool available in the global
   for (k = 0; k < arrayOfToolObjects.length; k++) {
     nameNumberArray.push(`${arrayOfToolObjects[k].name} : ${arrayOfToolObjects[k].id} : ${arrayOfToolObjects[k].phonenumber}`)
   }
 
+  //creates a card for each tool
   arrayOfToolObjects.map(  
     
     (eachToolObject)=>{
@@ -53,9 +60,11 @@ fetch("http://localhost:3000/tools")
   
 fetchAndRenderTools()
 
+//"click" event listener
 const selectButton = document.getElementById("reserveToolButton");
   selectButton.addEventListener("click", function() {
     const item = selectDropDown.value
+    
     const filteredArray = nameNumberArray.filter(string => string.includes(item));
     let last12 = filteredArray[0].substr(filteredArray[0].length - 12);
     alert(`You have successfully reserved a ${selectDropDown.value}\nPlease call ${last12} to arrange a pickup and dropoff`);
